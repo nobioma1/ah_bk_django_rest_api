a2a9973e62f675684a37fdd64f3ade7b792b0723
Noble Obioma <nobioma1@gmail.com>
2020-02-27 21:23:38 +0100

    User can delete books

--- bookstore_app/api/urls.py
+++ bookstore_app/api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   path('getbooks', views.get_books),
   path('addbook', views.add_book),
   path('updatebook/<int:book_id>', views.update_book),
+  path('deletebook/<int:book_id>', views.delete_book)
 ]
--- bookstore_app/api/views.py
+++ bookstore_app/api/views.py
@@ -58,3 +58,17 @@ def update_book(request, book_id):
       return JsonResponse({'error': str(e)}, safe=False, status=status.HTTP_404_NOT_FOUND)
     except Exception:
       return JsonResponse({'error': 'Something terrible went wrong'}, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+@api_view(["DELETE"])
+@csrf_exempt
+@permission_classes([IsAuthenticated])
+def delete_book(request, book_id):
+  user = request.user.id
+  try:
+    book = Book.objects.get(added_by=user, id=book_id)
+    book.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+  except ObjectDoesNotExist as e:
+    return JsonResponse({'error': str(e)}, safe=False, status=status.HTTP_404_NOT_FOUND)
+  except Exception:
+    return JsonResponse({'error': 'Something went wrong'}, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
