79fe610e9f2321f5a7a827a8a37959622a5ad3ef
Noble Obioma <nobioma1@gmail.com>
2020-02-27 21:17:21 +0100

    Add Auth scheme and permission policy

--- bookstore_app/bookstore_app/settings.py
+++ bookstore_app/bookstore_app/settings.py
@@ -80,6 +80,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bookstore_app.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
